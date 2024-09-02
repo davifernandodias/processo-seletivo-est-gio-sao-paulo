9c2f5b5998782b67e1eb6ea9459c10d0390529f9
davi dias <davifernandosousadias18@gmail.com>
2024-09-02 16:55:34 -0300

    feat: segundo exercicio resolvido

--- ex.py
+++ ex.py
@@ -3,3 +3,20 @@
 # retorna 91, pois interação while carrega 13 vezes e valor de soma
 # recebe a propria soma + k, sendo K = K + 1
 
+# fibonacci
+
+
+n = int(input("escreve n: "))
+a, b = 0, 1
+pertence = False # inicializando false!!!!
+
+for n in range(n + 1): 
+    if a == n:
+        pertence = True
+        break
+    a, b = b, a + b
+
+if pertence:
+    print(f"O número {pertence} pertence à sequência de fibonacci.")
+else:
+    print(f"O número {pertence} NÃO pertence à sequência de fibonnacci.")
