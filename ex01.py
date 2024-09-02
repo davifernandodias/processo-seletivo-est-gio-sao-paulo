02a0f6152effad67ddf73686caceb03ad6291fe9
davi dias <davifernandosousadias18@gmail.com>
2024-09-02 16:59:53 -0300

    refactor: alteração nas estruturas dos arquivos py

---
+++ ex01.py
@@ -0,0 +1,4 @@
+#first ex:
+
+# retorna 91, pois interação while carrega 13 vezes e valor de soma
+# recebe a propria soma + k, sendo K = K + 1
--- ex02.py
+++ ex02.py
@@ -1,11 +1,3 @@
-#first ex:
-
-# retorna 91, pois interação while carrega 13 vezes e valor de soma
-# recebe a propria soma + k, sendo K = K + 1
-
-# fibonacci
-
-
 n = int(input("escreve n: "))
 a, b = 0, 1
 pertence = False # inicializando false!!!!
